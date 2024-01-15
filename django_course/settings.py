22d940075a4bd56e4d2964dcc889449e4c535f33
Rasikrr <rs.t.95@mail.ru>
2024-01-16 00:16:38 +0600

    django-degug-toolbar was added for checking sql queries

--- django_course/settings.py
+++ django_course/settings.py
@@ -39,8 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    "debug_toolbar",
+
     "main",
-    "goods"
+    "goods",
+
 
 ]
 
@@ -52,6 +55,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
+
 ]
 
 ROOT_URLCONF = 'django_course.urls'
@@ -130,7 +136,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
-
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- django_course/urls.py
+++ django_course/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("main.urls", namespace="main")),
-    path("catalog/", include("goods.urls", namespace="catalog"))
+    path("catalog/", include("goods.urls", namespace="catalog")),
 ]
+
+if settings.DEBUG == True:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls"))
+    ]
